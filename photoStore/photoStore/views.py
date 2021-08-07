56e0e3ceed1d11aed8e7e8a868f1f5acef96031a
JasonAhn <jasonja.ahn@gmail.com>
2021-08-06 23:14:55 -0700

    Update views

--- photoStore/photoStore/views.py
+++ photoStore/photoStore/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import HttpResponse
 
 
 def index(request):
-<<<<<<< HEAD
+
     return HttpResponse('<h1>This is the home page</h1>')
-=======
-    return HttpResponse('This is the home page')
 
 
->>>>>>> db719f8de6e13e75f45272cf16c4f5039378ed5b
