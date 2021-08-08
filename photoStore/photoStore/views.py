6f2e00a54c9c34983652ec7e50ed5f1e28a300da
ahnrachel <rachel.ahnyr@gmail.com>
2021-08-07 22:39:39 -0700

    edit views.py

--- photoStore/photoStore/views.py
+++ photoStore/photoStore/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import HttpResponse
 
 
 def index(request):
-<<<<<<< HEAD
-    return HttpResponse('<h1>This is the home page</h1>')
-=======
-    return HttpResponse('This is the home page')
+    return HttpResponse("""
+    <h1>Welcome to ebook Store</h1>
+    <img src="http://i.ibb.co/Y7Yq32N/all-bk-set.png">
+    """)
 
 
->>>>>>> db719f8de6e13e75f45272cf16c4f5039378ed5b
+
+
