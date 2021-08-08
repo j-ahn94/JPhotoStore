591396f17bb6037a945f0e9766c8f444425d2525
JasonAhn <jasonja.ahn@gmail.com>
2021-08-07 22:56:19 -0700

    Create Store folder with django application

--- photoStore/photoStore/views.py
+++ photoStore/photoStore/views.py
@@ -2,16 +2,11 @@ from django.shortcuts import HttpResponse
 
 
 def index(request):
-<<<<<<< HEAD
+
     return HttpResponse("""
     <h1>Welcome to ebook Store</h1>
     <img src="http://i.ibb.co/Y7Yq32N/all-bk-set.png">
     """)
 
-
-=======
-
-    return HttpResponse('<h1>This is the home page</h1>')
->>>>>>> 56e0e3ceed1d11aed8e7e8a868f1f5acef96031a
 
 
