2591ac1338943bf757607342fdbbe8f3d59ddd06
JasonAhn <jasonja.ahn@gmail.com>
2021-08-08 20:30:19 -0700

    Make python migration

--- photoStore/photoStore/settings.py
+++ photoStore/photoStore/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Store.apps.StoreConfig'
+    'Store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
