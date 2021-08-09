0bffdc93b225d230e4a9e0749c3b8c08b40dfcc0
ahnrachel <rachel.ahnyr@gmail.com>
2021-08-08 20:25:29 -0700

    add class photo

--- photoStore/Store/models.py
+++ photoStore/Store/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class photo(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    cvr_url = models.CharField(max_length=2048)
+    
